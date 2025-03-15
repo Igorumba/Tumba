ccb34d93700d33d0da0599ff425d74b0c7c4141c
Igorumba <sklepuki@gmail.com>
2025-03-15 14:43:44 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,62 @@
-import sys
+import json
+
+from ui import Ui_MainWindow
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import *
-from convert import ConverterLogic
+from PyQt5 import QtWidgets
 
-class CurrencyConverter(QWidget):
-    def __init__(self):
-        super().__init__()
+app = QApplication([])
+MainWindow = QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
 
-        self.logic = ConverterLogic()
+notes = {}
 
-        self.initUI()
+def add_note():
+    note_name, ok = QInputDialog.getText(MainWindow, "Додати замітку", "Назва замітки: ")
+    if ok and note_name !="":
+        notes[note_name] = {"текст":"", "теги": []}
+        ui.listWidget.addItem(note_name)
+        #ui.listWidget_2.clear()
+        print(notes)
 
-    def initUI(self):
-        self.setWindowTitle('Конвертор валют')
-        self.relize(300, 200)
+def show_note():
+    key = ui.listWidget.selectedItems()[0].text()
+    ui.textEdit.setText(notes[key]["текст"])
+    ui.listWidget_2.clear()
+    ui.listWidget_2.addItems(notes[key]["теги"])
 
-        currencies =
+def save_note():
+    if ui.listWidget.selectedItem():
+        key = ui.listWidget.selectedItems()[0].text()
+        notes[key]["text"] = ui.textEdit.toPlainText()
+        with open("notes_data.json", "w") as file:
+            json.dump(notes,file,sort_keys = True, ensure_ascii = False)
+        print(notes)
+    else:
+        print("Замітка для збереження не вибрана!")
+
+def del_note():
+    if ui.listWidget.selectedItems():
+        key = ui.listWidget.selectedItems()[0].text()
+        del notes[key]
+        ui.listWidget.clear()
+        ui.listWidget_2.clear()
+        ui.textEdit.clear()
+        ui.listWidget.addItems(notes)
+        with open("notes_data.json", "w") as file:
+            json.dump(notes, file, sort_keys = True, ensure_ascii = False)
+        print(notes)
+    else:
+        print("Не вибрали замітку")
+
+        
+
+
+ui.pushButton.clicked.connect(add_note)
+
+
+
+MainWindow.show()
+app.exec_()
